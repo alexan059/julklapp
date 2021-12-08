71e8fdb46c3067cc42229a1e67f1baf352667095
alexan059 <developer.alexan059@gmail.com>
2021-12-08 19:52:04 +0100

    Added background particles

--- app.vue
+++ app.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
+  <div style="position: relative">
+    <div id="snow"/>
     <NuxtPage/>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted } from 'vue';
+import { tsParticles } from 'tsparticles';
+import { loadSnowPreset } from 'tsparticles-preset-snow';
 import { provideGroups } from '~/composables/useGroups';
 import { provideProfile } from '~/composables/useProfile';
 import useAuth from '~/composables/useAuth';
@@ -15,5 +18,22 @@ const { isLoggedIn } = useAuth();
 const [_, fetchGroups] = provideGroups();
 const [__, fetchUser] = provideProfile();
 
-onMounted(() => isLoggedIn() && Promise.all([fetchGroups(true), fetchUser(true)]));
+onMounted(() => {
+  isLoggedIn() && Promise.all([fetchGroups(true), fetchUser(true)]);
+
+  loadSnowPreset(tsParticles);
+  tsParticles.load('snow', {
+    preset: 'snow',
+    background: {
+      opacity: 0,
+    },
+  });
+});
 </script>
+
+<style lang="scss" scoped>
+#snow {
+  position: fixed;
+  z-index: -1;
+}
+</style>
